09841a14b35a2c4603fdb3169d5ba51fbd34238c
Bryan Fraschetti <bryan.fraschetti@canonical.com>
2025-05-15 11:46:08 +0200

    Fix: Refactor Matching and Help Msg on No Subcmd
    If no subcmd is supplied to dovetail, it will print the help message

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ mod subcmds;
 use subcmds::{show, list, run};
 
 fn main() {
-    let matches = Command::new("dovetail")
+    let cmd = Command::new("dovetail")
         .version("0.1.0")
         .about("The project agnostic workflow manager")
+        .arg_required_else_help(true) // This ensures help is shown if no subcommand is given
         .subcommand(
             Command::new("show")
                 .about("Displays the contents of the dovetail.yaml file or a specific environment")
@@ -29,9 +30,10 @@ fn main() {
                         .required(false)
                         .action(clap::ArgAction::SetTrue),
                 ),
-        )
-        .get_matches();
+        );
 
+    let matches = cmd.get_matches();
+    
     let content = match fs::read_to_string("dovetail.yaml") {
         Ok(c) => c,
         Err(e) => {
@@ -48,18 +50,19 @@ fn main() {
         }
     };
 
-    if let Some(show_matches) = matches.subcommand_matches("show") {
-        let env = show_matches.get_one::<String>("ENVIRONMENT");
-        show::run(&yaml, env);
-    }
-
-    if let Some(_list) = matches.subcommand_matches("list") {
-        list::run(&yaml);
-    }
-
-    if let Some(run_matches) = matches.subcommand_matches("run") {
-        let env = run_matches.get_one::<String>("ENVIRONMENT").unwrap();
-        let skip_prompt = run_matches.get_flag("yes");
-        run::run(&yaml, env, skip_prompt);
+    match matches.subcommand() {
+        Some(("show", show_matches)) => {
+            let env = show_matches.get_one::<String>("ENVIRONMENT");
+            show::run(&yaml, env);
+        }
+        Some(("list", _)) => {
+            list::run(&yaml);
+        }
+        Some(("run", run_matches)) => {
+            let env = run_matches.get_one::<String>("ENVIRONMENT").unwrap();
+            let skip_prompt = run_matches.get_flag("yes");
+            run::run(&yaml, env, skip_prompt);
+        }
+        _ => {} // No need for this case because of arg_required_else_help
     }
 }
