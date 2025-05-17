163ef0e263e4d5a59335678fc34229e8855b1858
Bryan Fraschetti <bryan.fraschetti@canonical.com>
2025-05-17 16:53:04 +0200

    Feat: Add Debian Packaging
    This commit adds a debian folder with some rules that enable packaging for
    Debian. I also added a short and non-comprehensive list of features I want to
    add

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/bryanfraschetti/dovetail/issues
+# Bug-Submit: https://github.com/bryanfraschetti/dovetail/issues/new
+# Changelog: https://github.com/bryanfraschetti/dovetail/blob/master/CHANGES
+# Documentation: https://github.com/bryanfraschetti/dovetail
+# Repository-Browse: https://github.com/bryanfraschetti/dovetail
+# Repository: https://github.com/bryanfraschetti/dovetail.git
